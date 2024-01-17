3544df465826e6fd87b6c585e2ffcca1dba65765
Jesse <jessecdenier@gmail.com>
2024-01-17 13:40:10 -0600

    Added courses page and simplified buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,3 @@
 function getElementById(id) {
   return document.querySelector("#" + id);
 }
-
-// Returns user to the home page.
-function goBack() {
-  window.location.href = "index.html";
-}
-
-// Adds event listener to Course Return button.
-courseReturnBtn.addEventListener("click", goBack);
